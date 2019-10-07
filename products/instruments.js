15ca9f7100585138e9c58921ede527618e309764
Erika Foin <erika.foin@gmail.com>
2019-10-07 16:43:15 -0700

    some instruments added

---
+++ products/instruments.js
@@ -0,0 +1,49 @@
+const flute = {
+    id: 'flute',
+    name: 'Silver Flute',
+    image: 'flute.png',
+    description: 'The voice of the angels',
+    category: 'woodwind',
+    price: 1000.00,
+    cost: 550.00
+};
+
+const oboe = {
+    id: 'oboe',
+    name: 'Wood Oboe',
+    image: 'oboe.png',
+    description: 'The voice of the duck',
+    category: 'woodwind',
+    price: 3000.00,
+    cost: 2000.00
+};
+
+const clarinet = {
+    id: 'clarinet',
+    name: 'Plastic Clarinet',
+    image: 'clarinet.png',
+    description: 'Marching band ready',
+    category: 'woodwind',
+    price: 250.00,
+    cost: 50.00
+};
+
+const bassClarinet = {
+    id: 'bass clarinet',
+    name: 'Bass clarinet',
+    image: 'bclar.png',
+    description: 'The sonorous voice of the deep',
+    category: 'woodwind',
+    price: 5000.00,
+    cost: 4000.00
+};
+
+const bassoon = {
+    id: 'bassoon',
+    name: 'Bassoon',
+    image: 'bassoon.png',
+    description: 'Grandpa is calling',
+    category: 'woodwind',
+    price: 9000.00,
+    cost: 8000.00
+};
