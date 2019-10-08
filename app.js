86129536564ce868a2d5b58fcbadefb17b7f9646
Erika Foin <erika.foin@gmail.com>
2019-10-07 20:14:42 -0700

    app.js fixed

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-import instrumentsData from './data/instruments.js';
+import instruments from '..data/instruments.js';
+import renderInstruments from './render-instruments.js';
+
+const instList = document.getElementById('instruments');
+
+for(let i = 0; i < instruments.length; i++) {
+    const instrument = instruments[i];
+    const dom = renderInstruments(instrument);
+    instList.appendChild(dom);
+}
