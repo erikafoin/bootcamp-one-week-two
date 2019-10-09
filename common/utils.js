e587d5c6f081c8f32834ea46c9bc3195c71fec21
Erika Foin <erika.foin@gmail.com>
2019-10-08 17:42:34 -0700

    util tests started

--- common/utils.js
+++ common/utils.js
@@ -6,3 +6,47 @@ const totalInstPrice = (instruments, quantity) => fruit.price * quantity;
 export const cartTotal = (instrument, cart) => {
 
 }
+
+export function findById(instruments, id) {
+    for (let i = 0; i < instruments.length; i++) {
+        const instrumentsById = instruments[i];
+    }
+}
+ export function calcLineTotal(quantity, price) {
+     const amount = quantity * price;
+     return roundCurrency(amount);
+ }
+
+
+const calcLineItem
+
+/*
+
+export function calcLineTotal(quantity, price) {
+    const amount = quantity * price;
+    return roundCurrency(amount);
+
+export function findById(items, id) {
+    // loop the array
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+
+        // check the id against item.id
+        if (item.id === id) {
+            return item;
+        }
+    }
+
+    // loop done, nothing found
+    return null;
+}
+
+export function toUSD(number) {
+    return number.toLocaleString('en-US', { 
+        style: 'currency', 
+        currency: 'USD' 
+    });
+}
+
+
+    */
---
+++ test/utils test.js
@@ -0,0 +1,53 @@
+import findById from '../common/utils.js';
+import calcLineItem from '../common/utils.js';
+
+const test = QUnit.test;
+
+QUnit.module('Render Instrument');
+
+test('renders an instrument', assert => {
+    // arrange
+    const flute = {
+        id: 'flute',
+        name: 'Silver Flute',
+        image: 'flute.jpeg',
+        description: 'The voice of the angels',
+        category: 'woodwind',
+        price: 1000.00,
+    };
+    const expected = '<li id="flute" class="woodwind" name="Silver Flute" title="The voice of the angels"><h2>Silver Flute</h2><img src="flute.jpeg" alt="flute image"><p class="price">$1000.00<button id="flute">Add to Cart</button></p></li>';
+    
+    // act
+    const dom = renderInstrument(flute);
+    const html = dom.outerHTML;
+    
+    // assert
+    assert.equal(html, expected);
+});
+
+
+test('renders a table row', assert => {
+    // arrange
+    const flute = {
+        id: 'flute',
+        name: 'Silver Flute',
+        image: 'flute.jpeg',
+        description: 'The voice of the angels',
+        category: 'woodwind',
+        price: 1000.00,
+    };
+    
+    const fluteOrder = {
+        id: flute,
+        quantity: 2,
+    };
+
+    const expected = '<tr><td>flute</td><td>2</td><td>$1000</td><td>$2000</td></tr>';
+    
+    // act
+    const instElementTr = renderTableRow(flute, fluteOrder);
+    const stringHtmlOfInstElement = instElementTr.outerHtml;
+    
+    // assert
+    assert.equal(stringHtmlOfInstElement, expected);
+});
